29725febfd847bb01e4bb34cd2db096af0fe3e9b
Stefani Dal Puppo <stefanipuppo@hotmail.com>
2024-05-03 14:05:24 -0300

    condicional ternaria

---
+++ src/ResultadoVestibular.java
@@ -0,0 +1,9 @@
+public class ResultadoVestibular {
+
+    public static void main(String[] args) {
+        int nota = 700;
+        String resultado = nota >= 700 ? "Aprovado" : "Reprovado";
+        
+        System.out.println(resultado);
+    }
+}
