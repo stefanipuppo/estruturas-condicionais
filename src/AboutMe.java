cf08535976f1e7737e645ad150aa4418e4e7f6ec
Stefani Dal Puppo <stefanipuppo@hotmail.com>
2024-05-04 04:33:34 -0300

    Try/Catch

---
+++ src/AboutMe.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Ola, me chamo " + nome.toUpperCase() + "" + sobrenome);
+        System.out.println("Tenho " + idade + "anos");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Os campos idade e altura precisam ser numericos");
+        }
+    }
+}
